6801e6420005c3383febcaaee6e3627e9f14fe51
Vardan Sargsyan <vardansargsyan92@gmail.com>
2018-02-14 16:25:07 +0400

    Added app icons. Created initial menu layout.

---
+++ Schedulee.Droid/Views/ReservationsActivity.cs
@@ -0,0 +1,37 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.Design.Widget;
+using Android.Support.V4.View;
+using Android.Support.V4.Widget;
+using Android.Support.V7.App;
+using Android.Support.V7.Widget;
+using Android.Views;
+
+namespace Schedulee.Droid.Views
+{
+    [Activity(Label = "Schedulee", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class ReservationsActivity : AppCompatActivity, NavigationView.IOnNavigationItemSelectedListener
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_reservations);
+            Toolbar toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+
+            DrawerLayout drawer = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+
+            ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, Resource.String.navigation_drawer_open, Resource.String.navigation_drawer_close);
+            drawer.AddDrawerListener(toggle);
+            toggle.SyncState();
+
+            NavigationView navigationView = FindViewById<NavigationView>(Resource.Id.nav_view);
+            navigationView.SetNavigationItemSelectedListener(this);
+        }
+
+        public bool OnNavigationItemSelected(IMenuItem menuItem)
+        {
+            return true;
+        }
+    }
+}
